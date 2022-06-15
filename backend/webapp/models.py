8a6a4a9a4ca8480a88e3c59edd5c9fc56adc5031
Greg Slomin <g@DESKTOP-E1EP6MO.localdomain>
2022-06-15 06:54:19 -0500

    Fixed webapp/migrations

--- backend/webapp/models.py
+++ backend/webapp/models.py
@@ -17,10 +17,4 @@ class Item(models.Model):
     type_id = models.IntegerField()
     location_flag = models.CharField(max_length=128)
     location_type = models.CharField(max_length=128)
-    loaded_dogma = models.BooleanField()
     character = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-class DogmaAttribute(models.Model):
-    attribute_id = models.IntegerField()
-    value = models.FloatField()
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -7,7 +7,7 @@ from pprint import pprint
 from esi.clients import EsiClientProvider
 esi = EsiClientProvider()
 
-from .models import Item, DogmaAttribute
+from .models import Item
 
 @token_required(scopes="esi-assets.read_assets.v1")
 def FetchAssets(request, token):
@@ -15,26 +15,15 @@ def FetchAssets(request, token):
     assets = esi.client.Assets.get_characters_character_id_assets(character_id=token.character_id,
                                                                   token=access_token).results()
 
-    item_ids = [x['item_id'] for x in assets]
-    asset_names = esi.client.Assets.post_characters_character_id_asset_names(character_id=token.character_id,
-                                                                             item_ids=item_ids,
-                                                                             token=access_token).results()
-    asset_name_map = {x['item_id']: x['name'] for x in asset_names}
-
     for asset in assets:
         item = Item(item_id=asset['item_id'],
                     type_id=asset['type_id'],
                     location_flag=asset['location_flag'],
                     location_id=asset['location_id'],
                     location_type=asset['location_type'],
-                    name=asset_name_map.get(asset['item_id'], ''),
                     character=request.user)
         item.save()
 
-        dogma = esi.client.Dogma.get_dogma_dynamic_items_type_id_item_id(type_id=item.type_id, item_id=item.item_id).results()
-        for attribute in dogma['dogma_attributes']:
-            DogmaAttribute(attribute_id=attribute['id'],
-                           value=attribute['value'],
-                           item=item).save()
+    pprint(assets)
 
     return HttpResponse(status=200)
